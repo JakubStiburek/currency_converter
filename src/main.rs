eee556b6c3c133f935f055bd2829467fb81fd7a4
Jakub Stibůrek <alcolp96@gmail.com>
2022-10-01 16:03:04 +0200

    mvp

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ use hyper::body::HttpBody as _;
 use tokio::io::{stdout, AsyncWriteExt as _};
 use clap::Parser;
 use hyper_tls::HttpsConnector;
+use serde::{Serialize, Deserialize};
+use serde_json::{Value};
 
 #[derive(Parser, Debug)]
 #[command(author = "Jakub Stiburek", version = "0.0.0", about = "Simple currency converter.")]
 struct Args {
-    #[arg(short, long, help = "The amount to be converted", default_value_t = 1)]
-    amount: u32,
+    #[arg(short, long, help = "The amount to be converted", default_value_t = 1.0)]
+    amount: f32,
     
     #[arg(short, long, help = "First currency code", default_value_t = String::from("eur"))]
     first: String,
@@ -20,28 +22,16 @@ struct Args {
     codes: bool,
 }
 
-// struct Code {
-//
-// }
-//
-// enum Codes {
-//     Eur,
-//     Usd,
-//     Czk,
-//     Hrn,
-//     Gbp
-// }
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
     let args = Args::parse();
     
-    println!("Hello {}!", args.first);
-    println!("Hello {}!", args.second);
+    println!("{}", args.first);
+    println!("{}", args.second);
     if args.codes {
-        println!("Hello codes")
+        println!("codes")
     }
     
     let uri = format!("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/{first}/{second}.json", first = args.first, second = args.second).parse()?;
@@ -50,9 +40,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     
     println!("Response: {}", resp.status());
     
+    let mut data = Vec::new();
     while let Some(chunk) = resp.body_mut().data().await {
-        stdout().write_all(&chunk?).await?;
+        data.extend(&chunk?)
     }
+    
+    let parsed: Value = serde_json::from_slice(&data)?;
+    let rate = parsed[&args.second].to_string();
+    
+    println!("{} {} is {} {}", args.amount, args.first, rate.parse::<f32>()? * args.amount, &args.second);
     
     Ok(())
 }
