71bb4974b02a06d4bee72dc5011bc333de8896ab
Jakub Stibůrek <alcolp96@gmail.com>
2022-10-28 22:08:47 +0200

    csv rates WIP

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,52 @@
+use std::error::Error;
+use std::fmt::format;
+use std::fs::{File};
+use std::path::PathBuf;
+use std::collections::HashMap;
+use std::fmt;
 use hyper::{Client};
 use hyper::body::HttpBody as _;
 use clap::{Parser, ValueEnum};
 use hyper_tls::HttpsConnector;
 use serde_json::{Value};
-use std::fmt;
 use tokio::time::{timeout, Duration};
 
-#[derive(Parser, Debug)]
-#[command(author = "Jakub Stiburek", version = "0.0.0", about = "Simple currency converter.")]
-struct Args {
-    amount: Option<f32>,
-    
-    first: Option<Code>,
-    
-    second: Option<Code>,
-}
+type Record = HashMap<String, String>;
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
 enum Code {
-    Czk,
-    Eur,
-    Usd,
-    Gbp,
-    Pln
+    EUR,
+    CHF,
+    SGD,
+    HKD,
+    INR,
+    BRL,
+    JPY,
+    ISK,
+    MXN,
+    MYR,
+    NOK,
+    BGN,
+    HUF,
+    CZK,
+    HRK,
+    THB,
+    RON,
+    USD,
+    TRY,
+    GBP,
+    AUD,
+    CAD,
+    SEK,
+    KRW,
+    DKK,
+    PHP,
+    CNY,
+    IDR,
+    ILS,
+    ZAR,
+    NZD,
+    PLN,
 }
 
 impl fmt::Display for Code {
@@ -31,77 +55,30 @@ impl fmt::Display for Code {
     }
 }
 
-static CODES: [&str; 5] = ["czk", "eur", "usd", "gbp", "pln"];
-
-struct Pair {
-    first: String,
-    second: String
+struct Rate {
+    code: Code,
+    value: f32,
 }
 
-impl Pair {
-    fn new(first: String, second: String) -> Self {
-        Self {
-            first,
-            second
-        }
-    }
+fn main() {
+    load_rates();
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let https = HttpsConnector::new();
-    let client = Client::builder().build::<_, hyper::Body>(https);
-    let args = Args::parse();
+fn load_rates() -> Result<(), Box<dyn Error>> {
+    let mut home_dir: PathBuf = dirs::home_dir().unwrap();
+    home_dir.push("eurofxref.csv");
     
-    let first = match args.first {
-        Some(c) => c.to_string().to_lowercase(),
-        None => Code::Eur.to_string().to_lowercase()
-    };
-    let second = match args.second {
-        Some(c) => c.to_string().to_lowercase(),
-        None => Code::Czk.to_string().to_lowercase()
-    };
-    let amount = match args.amount {
-        Some(a) => a,
-        None => 1.0
-    };
-    
-    let mut pairs: Vec<Pair> = vec![];
-    
-    for i in CODES.iter() {
-        for j in  CODES.iter(){
-            pairs.push(Pair::new(i.to_string(), j.to_string()))
+    let file = File::open(home_dir)?;
+    let mut rdr = csv::Reader::from_reader(file);
+    let rates: Vec<String> = vec![];
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        for (key, value) in record.iter() {
+            // todo record date
+            if (key.len() > 1 && !key.eq("Date")) {
+                println!("{:?} - {:?}", key.trim(), value.trim().parse::<f32>()?)
+            }
         }
-    }
-    
-    let mut uris: Vec<String> = vec![];
-    
-    for i in pairs.iter() {
-        uris.push(format!("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/{}/{}.json", &i.first, &i.second));
-    }
-    
-    let mut rates: Vec<f32> = vec![];
-    
-    for uri in uris.iter() {
-        let mut resp = client.get(uri.parse()?).await?;
-        
-        println!("{}", &uri);
-    
-        let mut data = Vec::new();
-        while let Some(chunk) = resp.body_mut().data().await {
-            data.extend(&chunk?)
-        }
-    
-        let parsed: Value = serde_json::from_slice(&data)?;
-        let rate = parsed[&second].to_string();
-        
-        println!("{}", &rate);
-        
-        rates.push(rate.parse::<f32>()? * &amount);
-    }
-    
-    for r in rates {
-        println!("{}", r);
     }
     
     Ok(())
